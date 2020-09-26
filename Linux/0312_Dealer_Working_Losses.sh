178c7b4f193e3a307ca74346b2ab683f83c5eba4
Brookras <67078970+Brookras@users.noreply.github.com>
2020-09-26 16:15:35 -0700

    Create 0312_Dealer_Working_Losses.sh

---
+++ Linux/0312_Dealer_Working_Losses.sh
@@ -0,0 +1,5 @@
+cat 0312_Dealer_schedule | awk -F" " '{print $1, $2, $5. $6}' | grep '05:00:00 AM' >> Dealer_Working_during Losses_0312
+cat 0312_Dealer_schedule | awk -F" " '{print $1, $2, $5. $6}' | grep '08:00:00 AM' >> Dealer_Working_during Losses_0312
+cat 0312_Dealer_schedule | awk -F" " '{print $1, $2, $5. $6}' | grep '02:00:00 PM' >> Dealer_Working_during Losses_0312
+cat 0312_Dealer_schedule | awk -F" " '{print $1, $2, $5. $6}' | grep '08:00:00 PM' >> Dealer_Working_during Losses_0312
+cat 0312_Dealer_schedule | awk -F" " '{print $1, $2, $5. $6}' | grep '11:00:00 PM' >> Dealer_Working_during Losses_0312
